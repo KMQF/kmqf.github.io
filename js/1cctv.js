d77e36709f7ed4bc72d171c4c8d34f0769aefb46
KMQF <3120930696@qq.com>
2022-01-14 23:26:36 +0800

    1

---
+++ js/1cctv.js
@@ -0,0 +1,28 @@
+<script>
+	var cCount=0;
+	document.getElementById("menu").hidden="hidden"
+	function sixClick(){
+			if(cCount==6){
+				sixClickDisplay();
+			}else if(cCount==7){
+				sevenClickHidden();
+			}else if(cCount==10){
+				alert(cCount+"休息一下吧!");
+				location.reload();
+			}else {
+				clickCount();
+			}
+		} 
+	function clickCount(){ 
+		cCount=cCount+1; 
+	} 
+	
+	function sixClickDisplay(){ 
+		document.getElementById("menu").hidden="";
+		cCount=cCount+1; 
+	}
+	function sevenClickHidden(){
+			document.getElementById("menu").hidden="hidden";
+			cCount=cCount+1;
+	}
+</script>
