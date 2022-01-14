60de7e5c3a1dc5056a01809059e4ff1acbfc303b
KMQF <3120930696@qq.com>
2022-01-14 23:34:00 +0800

    js

--- js/1cctv.js
+++ js/1cctv.js
@@ -1,28 +1,26 @@
-<script>
-	var cCount=0;
-	document.getElementById("menu").hidden="hidden"
-	function sixClick(){
-			if(cCount==6){
-				sixClickDisplay();
-			}else if(cCount==7){
-				sevenClickHidden();
-			}else if(cCount==10){
-				alert(cCount+"休息一下吧!");
-				location.reload();
-			}else {
-				clickCount();
-			}
-		} 
-	function clickCount(){ 
-		cCount=cCount+1; 
+var cCount=0;
+document.getElementById("menu").hidden="hidden"
+function sixClick(){
+		if(cCount==6){
+			sixClickDisplay();
+		}else if(cCount==7){
+			sevenClickHidden();
+		}else if(cCount==10){
+			alert(cCount+"休息一下吧!");
+			location.reload();
+		}else {
+			clickCount();
+		}
 	} 
-	
-	function sixClickDisplay(){ 
-		document.getElementById("menu").hidden="";
-		cCount=cCount+1; 
-	}
-	function sevenClickHidden(){
-			document.getElementById("menu").hidden="hidden";
-			cCount=cCount+1;
-	}
-</script>
+function clickCount(){ 
+	cCount=cCount+1; 
+} 
+
+function sixClickDisplay(){ 
+	document.getElementById("menu").hidden="";
+	cCount=cCount+1; 
+}
+function sevenClickHidden(){
+		document.getElementById("menu").hidden="hidden";
+		cCount=cCount+1;
+}
